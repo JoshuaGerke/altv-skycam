2757be77a401de9816abf93d67424608e4739b0e
Joshua Gerke <37521388+JoshuaGerke@users.noreply.github.com>
2023-01-05 17:52:30 +0100

    fix: native update

--- skycam.js
+++ skycam.js
@@ -4,22 +4,32 @@ import * as native from 'natives';
 alt.onServer('skycam:Move', moveFromToAir);
 
 function moveFromToAir(moveTo, switchType) {
-    /*
-        switchType: 0 - 3
+  /*
+          switchType: 0 - 3
 
-        0: 1 step towards player
-        1: 3 steps out from player (Recommended)
-        2: 1 step out from player
-        3: 1 step towards player
+          0: 1 step towards player
+          1: 3 steps out from player (Recommended)
+          2: 1 step out from player
+          3: 1 step towards player
+     */
+
+  /* [SERVERSIDE EXAMPLE]
+
+      //TO SKY LIKE IN GTA ONLINE
+      alt.emitClient(player, 'skycam:Move', 'up', 1); 
+
+      //TO PLAYER LIKE IN GTA ONLINE
+      alt.emitClient(player, 'skycam:Move', 'up', 1);
+
     */
-    switch (moveTo) {
-        case 'up':
-            native.switchOutPlayer(alt.Player.local.scriptID, 0, parseInt(switchType));
-            break;
-        case 'down':
-            native.switchInPlayer(alt.Player.local.scriptID);
-            break;
-        default:
-            break;
-    }
+  switch (moveTo) {
+    case 'up':
+      native.switchToMultiFirstpart(alt.Player.local.scriptID, 0, parseInt(switchType));
+      break;
+    case 'down':
+      native.switchToMultiSecondpart(alt.Player.local.scriptID);
+      break;
+    default:
+      break;
+  }
 }
